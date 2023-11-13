06ed26ca0313801cc9663cfa4df87ef4275b90bf
DuDuongVanPtit <duduongvanptit@gmail.com>
2023-11-13 11:07:49 +0700

    13/11/2023

--- src/main/java/dudv/vn/java_big_assignment/controller/home/HomeController.java
+++ src/main/java/dudv/vn/java_big_assignment/controller/home/HomeController.java
@@ -7,8 +7,11 @@ import dudv.vn.java_big_assignment.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("home")
@@ -29,8 +32,26 @@ public class HomeController {
         return "/user/createUser.html";
     }
 
+    @RequestMapping("/login")
+    String loginForm(){
+        return "/user/userLogin.html";
+    }
+
     @RequestMapping(method = RequestMethod.POST, value = "/save")
-    String register(@ModelAttribute UserDto userDto, Model model){
+    String register(@Valid @ModelAttribute UserDto userDto,BindingResult bindingResult, Model model, RedirectAttributes
+            redirectAttributes){
+        if(bindingResult.hasErrors()){
+            System.out.println("lỗi nhập vào");
+            return "/user/createUser.html";
+        }
+        if(userService.getDetailByPhone(userDto.getPhoneNumber()) != null){
+            model.addAttribute("phoneError", "Phone number already exists");
+            return "/user/createUser.html";
+        }
+        if(userDto.getPassword().compareTo(userDto.getRePassword()) != 0){
+            model.addAttribute("passwordError", "Passwords do not match");
+            return "/user/createUser.html";
+        }
         userService.addUser(userDto);
         return "/home/home.html";
     }
